091c594a638a77819acf01d92aec8982405594dc
mrjojo <jonasj76@gmail.com>
2010-11-11 19:37:07 +0100

    Opens an OpenGL window

--- src/main.c
+++ src/main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <GL/glfw.h>
+
+#define WIDTH  640
+#define HEIGHT 480
 
 int main (void)
 {
-  printf ("glTetris\n");
+   /* Initalize GLFW */
+   if (!glfwInit())
+   {
+      printf ("error: glfwInit() failed.\n");
+      exit (EXIT_FAILURE);
+   }
 
-  return 1;
+   /* Register exit function */
+   if (atexit (glfwTerminate))
+   {
+      printf ("error: atexit() failed.\n");
+      exit (EXIT_FAILURE);
+   }
+
+   /* Open an OpenGL window */
+   if (!glfwOpenWindow (WIDTH, HEIGHT, 0, 0, 0, 0, 0, 0, GLFW_WINDOW))
+   {
+      printf ("error: glfwOpenWindow() failed.\n");
+      exit (EXIT_FAILURE);
+   }
+
+   /* Main loop. Wait until ESC key was pressed or window was closed */
+   while (GLFW_PRESS != glfwGetKey (GLFW_KEY_ESC) && glfwGetWindowParam (GLFW_OPENED))
+   {
+      glfwSwapBuffers();
+   }
+
+   /* Close window and and terminate */
+   glfwTerminate();
+   exit (EXIT_SUCCESS);
 }
+
+/**
+* Local Variables:
+* version-control: t
+* c-file-style: "ellemtel"
+* End:
+*/
